275e1c1077a6350f118c1197d0f815df2a7ee743
Kyo <33697576+kyo1337@users.noreply.github.com>
2020-04-06 03:59:05 +0700

    Update quotesmaker.php

--- quotesmaker.php
+++ quotesmaker.php
@@ -6,13 +6,10 @@ $file_arr = file($file);
 $num_lines = count($file_arr);
 // Getting the last array index number by subtracting 1 as the array index starts from 0
 $last_arr_index = $num_lines - 1;
-
 // Random index number
 $rand_index = rand(0, $last_arr_index);
-
 // random text from a line. The line will be a random number within the indexes of the array
 $rand_text = $file_arr[$rand_index];
-
 echo $rand_text;
 
 ?>
